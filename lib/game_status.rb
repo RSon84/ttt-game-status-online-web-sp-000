0834f2840bb71ed79272bd7c94b38f76af0a298c
Devin Wilson <craigvin84@yahoo.com>
2020-08-29 18:13:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,10 +18,10 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8],
 def won?(board)
   if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     return false
-end
-  if
+else
     board.select do |index|
       index.any? != WIN_COMBINATIONS
       return false
   end
+end
 end
